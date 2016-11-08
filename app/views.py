54a3779c73190015780188c956d51ff819cac77a
mpeng8 <mpeng8@gatech.edu>
2016-11-07 20:48:05 -0500

    add UI for main page

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
+from flask import render_template, url_for
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    print(url_for('static', filename='style.css'));
+    return render_template('index.html')
