19bafa408892302331323c6a75859a962a3bd449
mpeng8 <mpeng8@gatech.edu>
2016-11-10 22:59:09 -0500

    handle login

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,9 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template,session
+from datetime import timedelta
 
 app = Flask(__name__)
+#set session time
+app.permanent_session_lifetime = timedelta(seconds=180)
+
+app.config.from_object('config')
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,8 +1,33 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, redirect,session,abort, request, flash
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    print(url_for('static', filename='style.css'));
-    return render_template('index.html')
+    if not session.get('logged_in'):
+    	return render_template('index.html')
+    else:
+	return render_template('dashboard.html')
+    
+
+@app.route('/login', methods = ['GET','POST'])
+def login():
+    return render_template('login.html')
+
+
+@app.route('/auth', methods = ['POST'])
+def userLogIn():
+    error = None
+    print(request.form['password'])
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    else:
+        error = 'Invalid Credentials'
+	return render_template('login.html', error = error)
+    return index()
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return index()
