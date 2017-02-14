60d3436bf370b1a6cdc2c0597ed50f2d43a94cfb
Mengyi Peng <rachel.peng.nico@gmail.com>
2017-02-14 01:07:51 -0500

    Finish line chart v0

--- app/static/assets/js/stockDisplay.js
+++ app/static/assets/js/stockDisplay.js
@@ -1,5 +1,4 @@
 
-  //var data = [4, 8, 15, 16, 23, 42];
 var json = $.getJSON("https://www.quandl.com/api/v3/datasets/YAHOO/MSFT.json?start_date=2015-01-03&end_date=2015-02-03&order=asc");
 var stockCodes;
 json.complete(function() {
@@ -14,23 +13,25 @@ var stockData = stockCodes.dataset.data;
 
 var parseDate = d3.time.format("%y-%b-%d").parse;
 
-
 //d3 margin convention
-var margin = {top: 20, right: 80, bottom: 50, left: 100},
-width = 750,
-height = 500 - margin.top-margin.bottom;
+var margin = {top: 20, right: 20, bottom: 30, left: 35},
+    width = 660 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
 // Set the ranges
 var	x = d3.time.scale().range([0,width]);
-var	y = d3.time.scale().range([height, 0]);
+var	y = d3.scale.linear().range([height, 0]);
+
 var line = d3.svg.line()
     .x(function(d) { return x(d[0]); })
     .y(function(d) { return y(d[4]); });
 
 stockData.forEach(function(d) {
         d[0] = new Date(d[0]);
-        console.log(d[0]);
+        d[4] = +d[4];
+        console.log(d[4]);
     });
+
 // Define the axes
 x.domain(d3.extent(stockData, function(d) { return d[0]; }));
 y.domain(d3.extent(stockData, function(d) { return d[4]; }));
@@ -42,30 +43,40 @@ var	yAxis = d3.svg.axis().scale(y)
 
 
 var svg = d3.select(".chart").append("svg")
-  .attr("width", 750)
-  .attr("height", 500)
-  .style("border", "1px solid black");
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+
+  svg.append("rect")
+      .attr("width", "100%")
+      .attr("height", "100%")
+      .attr("fill", "white");
 
     // Add the X Axis
 	svg.append("g")
 		.attr("class", "x axis")
-		.attr("transform", "translate(0," + height + ")")
+		.attr("transform", "translate(" + margin.left + "," + (margin.top + height) + ')')
 		.call(xAxis);
+
 
 	// Add the Y Axis
 	svg.append("g")
-		.attr("class", "y axis")
-    .attr("transform", "translate(" + width + ",0)")
-		.call(yAxis)
+		    .attr("class", "y axis")
+        .attr("transform", "translate(" + margin.left + ','+ margin.top + ')')
+		    .call(yAxis)
     .append("text")
+        .attr("transform", "rotate(-90)")
     .attr("y", 6)
     .attr("dy", ".71em")
+    .attr("fill", "black")
     .style("text-anchor", "end")
     .text("Price ($)");
 
-    svg.append("path")
-        .datum(stockData)
+  svg.append("path")
+     .datum(stockData)
         .attr("class", "line")
+     .attr("transform", "translate(" + margin.left + ','+ margin.top + ')')
         .attr("d", line);
 
 
