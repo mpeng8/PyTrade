5c768552d5022a93fb470f9dcf6eccb97bb03831
Mengyi Peng <rachel.peng.nico@gmail.com>
2017-02-20 19:40:54 -0500

    Remove one plugin & change default behabior

--- app/static/assets/js/stockSeries.js
+++ app/static/assets/js/stockSeries.js
@@ -13,7 +13,7 @@ var stockID = document.getElementById("stockID_info").innerText;
 var todayDate = new Date();
 //console.log(todayDate);
 var oldDate = new Date();
-oldDate.setDate(todayDate.getDate() - 14);
+oldDate.setDate(todayDate.getDate() - 30);
 //console.log(oldDate);
 
 var dateLT = todayDate.toJSON().slice(0,10).replace(/-/g,'');
@@ -24,35 +24,34 @@ $.ajax({
     crossOrigin: true,
     url: "https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?date.gte="+dateGTE+"&date.lt=" + dateLT+"&ticker=" + stockID+"&api_key=sXBsPYtsefUf_qaFxNDK",
     success: function(data) {
-        //console.log(data);
-        var stockCodes = JSON.parse(data);
-        var length = stockCodes.datatable.data.length;
-        var stockData = [];
-        for (var i = 0; i < length; i++) {
-          stockData.push(new stock(stockCodes.datatable.data[i]));
+      //console.log(data);
+      var stockCodes = JSON.parse(data);
+      var length = stockCodes.datatable.data.length;
+      var stockData = [];
+      for (var i = 0; i < length; i++) {
+        stockData.push(new stock(stockCodes.datatable.data[i]));
           //console.log(stockData[i]);
-        }
+      }
+      //d3 margin convention
+      var margin = {top: 20, right: 20, bottom: 30, left: 35},
+          width = 770 - margin.left - margin.right,
+          height = 400 - margin.top - margin.bottom;
 
-        //d3 margin convention
-        var margin = {top: 20, right: 20, bottom: 30, left: 35},
-            width = 770 - margin.left - margin.right,
-            height = 400 - margin.top - margin.bottom;
+      var xScale = d3.time.scale(),
+          yScale = d3.scale.linear();
 
-        var xScale = d3.time.scale(),
-            yScale = d3.scale.linear();
+      var xAxis = d3.svg.axis()
+          .scale(xScale)
+          .orient('bottom')
+          .ticks(5);
 
-        var xAxis = d3.svg.axis()
-            .scale(xScale)
-            .orient('bottom')
-            .ticks(5);
+      var yAxis = d3.svg.axis()
+          .scale(yScale)
+          .orient('left');
 
-        var yAxis = d3.svg.axis()
-            .scale(yScale)
-            .orient('left');
-
-        var series = candlestick ()
-            .xScale(xScale)
-            .yScale(yScale);
+      var series = candlestick ()
+          .xScale(xScale)
+          .yScale(yScale);
 
         // var gridlines = gridlines ()
         //     .xScale(xScale)
@@ -60,55 +59,51 @@ $.ajax({
         //     .xTicks(10)
         //     .yTicks(5);
 
-        var svg = d3.select('.chart').append('svg')
-                .attr('width', width + margin.left + margin.right)
-                .attr('height', height + margin.top + margin.bottom);
+      var svg = d3.select('.chart').append('svg')
+                  .attr('width', width + margin.left + margin.right)
+                  .attr('height', height + margin.top + margin.bottom);
 
         // Create chart
-        var g = svg.append('g')
-            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+      var g = svg.append('g')
+                 .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-        // Create plot area
-        var plotArea = g.append('g');
+      // Create plot area
+      var plotArea = g.append('g');
 
+      plotArea.append('clipPath')
+              .attr('id', 'plotAreaClip')
+              .append('rect')
+              .attr({ width: width, height: height });
 
-
-        plotArea.append('clipPath')
-                .attr('id', 'plotAreaClip')
-                .append('rect')
-                .attr({ width: width, height: height });
-
-        plotArea.attr('clip-path', 'url(#plotAreaClip)');
-
-
+      plotArea.attr('clip-path', 'url(#plotAreaClip)');
 
         // Set scale domains
-        var minN = d3.min(stockData, function (d) { return d.date; }).getTime(),
-            maxN = d3.max(stockData, function (d) { return d.date; }).getTime();
-        var minDate = new Date(minN - 8.64e7),
-            maxDate = new Date(maxN + 8.64e7);
-        var yMin = d3.min(stockData, function (d) { return d.low; }),
-            yMax = d3.max(stockData, function (d) { return d.high; });
+      var minN = d3.min(stockData, function (d) { return d.date; }).getTime(),
+          maxN = d3.max(stockData, function (d) { return d.date; }).getTime();
+      var minDate = new Date(minN - 8.64e7),
+          maxDate = new Date(maxN + 8.64e7);
+      var yMin = d3.min(stockData, function (d) { return d.low; }),
+          yMax = d3.max(stockData, function (d) { return d.high; });
 
-       // There are 8.64e7 milliseconds in a day.
-       xScale.domain([
+        // There are 8.64e7 milliseconds in a day.
+      xScale.domain([
           new Date(maxDate.getTime() - (8.64e7 * 31.5)),
           new Date(maxDate.getTime() + 8.64e7)
-       ]);
+      ]);
 
-        yScale.domain(
+      yScale.domain(
           [
               d3.min(stockData, function (d) {
                 return d.low;
               }),
               d3.max(stockData, function (d) {
                 return d.high;
-              })
-            ]
-        ).nice();
+            })
+          ]
+      ).nice();
 
       // Set scale ranges
-        xScale.range([0, width]);
+      xScale.range([0, width]);
       yScale.range([height, 0]);
       // Draw axes
       g.append('g')
@@ -198,15 +193,12 @@ $.ajax({
           zoom.scaleExtent([minScale, maxScale]);
       }
 
-
       var viewport = d3.svg.brush()
       .x(navXScale)
       .on("brush", function () {
           xScale.domain(viewport.empty() ? navXScale.domain() : viewport.extent());
           redrawChart();
       });
-
-
 
       var zoom = d3.behavior.zoom()
       .x(xScale)
@@ -223,23 +215,34 @@ $.ajax({
       });
 
       var overlay = d3.svg.area()
-      .x(function (d) { return xScale(d.date); })
-      .y0(0)
-      .y1(height);
+          .x(function (d) { return xScale(d.date); })
+          .y0(0)
+          .y1(height);
 
-  plotArea.append('path')
-      .attr('class', 'overlay')
-      .attr('d', overlay(stockData))
-      .call(zoom);
+      plotArea.append('path')
+          .attr('class', 'overlay')
+          .attr('d', overlay(stockData))
+          .call(zoom);
 
       viewport.on("brushend", function () {
-              updateZoomFromChart();
-          });
+          updateZoomFromChart();
+      });
 
       navChart.append("g")
           .attr("class", "viewport")
           .call(viewport)
           .selectAll("rect")
           .attr("height", navHeight);
+
+      var daysShown = 12;
+
+      xScale.domain([
+          stockData[stockData.length - daysShown - 1].date,
+          stockData[stockData.length - 1].date
+      ]);
+
+      redrawChart();
+      updateViewportFromChart();
+      updateZoomFromChart();
     }
 });
