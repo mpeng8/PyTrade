49380eead88e54d1d0003a3bf0c9d5889e4d6e3c
Mengyi Peng <rachel.peng.nico@gmail.com>
2017-04-23 01:22:16 -0400

    N

---
+++ package_install.py
@@ -0,0 +1,5 @@
+import conda.cli
+conda.cli.main('conda', 'install',  '-y', 'numpy')
+conda.cli.main('conda', 'install',  '-y', 'pandas')
+conda.cli.main('conda', 'install', '-y', 'ta-lib')
+conda.cli.main('conda', 'install', '-y', 'scikit-learn')
