ee2e92bfc5788bf11e1f11f64f3e66b54813485b
mxie33 <mxie33@gatech.edu>
2017-04-24 20:47:04 -0400

    Update package_install.py

--- package_install.py
+++ package_install.py
@@ -1,6 +1,6 @@
 import conda.cli
 conda.cli.main('conda', 'install',  '-y', 'numpy')
 conda.cli.main('conda', 'install',  '-y', 'pandas')
-conda.cli.main('conda', 'install', '-y', 'ta-lib')
+conda.cli.main('conda', 'install', '-c','quantopian', 'ta-lib=0.4.9')
 conda.cli.main('conda', 'install', '-y', 'scikit-learn')
 conda.cli.main('conda', 'install', '-c', 'sqlalchemy=1.1.9')
